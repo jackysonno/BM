2be993a8ad9c13dce99932f74cc923df19f298b4
jackysonno <jackchenkj@gmail.com>
2026-07-21 20:27:08 +0330

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ir.biomaze.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "ir.biomaze.app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
